8f9c8f6a33a91a2a206b6a6873856d4e505efef9
Alexander Krupka <bamaboy483@gmail.com>
2022-02-01 20:26:01 +0200

    Update google-auth.js

--- helpers/google-auth.js
+++ helpers/google-auth.js
@@ -19,7 +19,6 @@ passport.use(
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
       callbackURL: `${LINK}`,
-      scope: ["profile", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
       const email = profile._json.email;
